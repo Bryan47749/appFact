499773a6f264ccecf836bc6d4aa6d445e69b7de2
Bryan Toalumbo R <btoalumbo@crtvmq.local>
2022-07-19 20:40:45 -0500

    aa

---
+++ src/app/resources/URL.ts
@@ -0,0 +1,12 @@
+export abstract class URL{
+    //UserClient
+    public static USER_LOGIN = "http://serviciosFactura.somee.com/APISERVICE/api/userClient/login";
+    public static CLIENTS = "http://serviciosFactura.somee.com/APISERVICE/api/clients";
+    //Products
+    public static PRODUCT = "http://serviciosFactura.somee.com/APISERVICE/api/product";
+    //Sale
+    public static SALE = "http://serviciosFactura.somee.com/APISERVICE/api/sale";
+    public static SALE_DETAILS = "http://serviciosFactura.somee.com/APISERVICE/api/saleDetails";
+    //UserAdmin 
+    public static ADMIN_LOGIN = "http://serviciosFactura.somee.com/APISERVICE/api/UserAdmin/login";
+}
